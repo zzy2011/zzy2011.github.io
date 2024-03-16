93f6bff8ccf67ee5902abf9d3d1799c7e6e1e4a6
zzy2011 <zzy-_-@outlook.com>
2024-03-16 08:42:57 +0800

    更新：添加解密窗口

--- js/cipher.js
+++ js/cipher.js
@@ -1,20 +1,44 @@
+function formatOffset(offset) {
+    if (offset >= 0) {
+        return offset % 26;
+    } else {
+        return 26 + offset % 26;
+    }
+}
+
+function encrypt(word, offset) {
+    offset = formatOffset(offset);
+    let ciphertext = "";
+    for (const ch of word) {
+        if ("A" <= ch && ch <= "Z") {
+            ciphertext +=
+                String.fromCharCode((ch.charCodeAt() - "A".charCodeAt() +
+                offset) % 26 + "A".charCodeAt());
+        } else if ("a" <= ch && ch <= "z") {
+            ciphertext += String.fromCharCode((ch.charCodeAt() - "a".charCodeAt() +
+                offset) % 26 + "a".charCodeAt());
+        } else {
+            ciphertext += ch;
+        }
+    }
+
+    return ciphertext;
+}
+
+function decrypt(word, offset) {
+    return encrypt(word, -offset);
+}
+
 function handleEncrypt() {
     let form = document.forms["encryption-parameters-form"];
     let plaintext = form.plaintext.value;
     let offset = Number(form.offset.value);
-    form.ciphertext.value = "";
-    for (ch of plaintext) {
-        let charCode;
-        if ("A" <= ch && ch <= "Z") {
-            charCode = (ch.charCodeAt() - "A".charCodeAt() + offset) % 26 +
-                "A".charCodeAt();
-        } else if ("a" <= ch && ch <= "z") {
-            charCode = (ch.charCodeAt() - "a".charCodeAt() + offset) % 26 +
-                "a".charCodeAt();
-        } else {
-            charCode = ch.charCodeAt();
-        }
+    form.ciphertext.value = encrypt(plaintext, offset);
+}
 
-        form.ciphertext.value += String.fromCharCode(charCode);
-    }
+function handleDecrypt() {
+    let form = document.forms["decryption-parameters-form"];
+    let ciphertext = form.ciphertext.value;
+    let offset = Number(form.offset.value);
+    form.plaintext.value = decrypt(ciphertext, offset);
 }
